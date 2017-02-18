44e3f2d5fe7ea9b92433c34939df0d778680acd8
dandaryll <dandaryll88@gmail.com>
2017-02-18 11:21:10 -0800

    create recommendations.js

--- script/newUser.js
+++ script/newUser.js
@@ -91,57 +91,5 @@ function calculateCalories(weights, height, age, gender, activityLevel)
 // newWeight()
 
 // recommendations()
-function recommendations() {
-  var goalForStatus = calculateCalories(userData.weights, userData.height, userData.age, userData.gender, userData.activityLevel);
-  //var goalLimiter = userData.goalWeight;
-  var text = "";
-
-    // FIXME: 2000 cannot be goalForStatusTest because same variable cannot be compared
-    if (goalForStatus >= 1800 && goalForStatus < 2000) {
-      text = "Good";
-    }
-    else if  (goalForStatus >= 1500) {
-      text = "Medium";
-    }
-    else if (goalForStatus >= 1200 || goalForStatus < 1200) {
-      text = "Bad";
-    }
-
-    return text;
-}
-
-// TESTED WORKING! BODY MASS INDEX ALT. FOR RECOMMENDATION FUNCTION
-function BMI() {
-  var height = userData.height;
-  var weight = userData.weights;
-
-  // Convert height to centimeters to work with formula below
-  height = height * 30.48; // feet to centimeters
-
-  // Convert weight from kilograms to pounds to work with formula below
-  weight = weight / 2.20462; // lbs. to kg
-
-  return ((weight) / Math.pow((height/100), 2));
-
-}
-
-// Source: https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/
-function bmiScale () {
-  var bmiValue = BMI();
-  var text = "";
-  if (bmiValue < 18.5) {
-      text = "Your BMI scale is considered underweight";
-  }
-  else if (bmiValue <= 24.9) {
-    text = "Your BMI scale is considered normal or healthy weight";
-  }
-  else if (bmiValue <= 25.0 || bmiValue <= 29.9) {
-    text = "Your BMI scale is considered overweight";
-  }
-  else if (bmiValue >= 30.0) {
-    text = "Your BMI scale is considered obese";
-  }
-  return text;
-}
 
 // isReachGoal()
--- script/recommendations.js
+++ script/recommendations.js
@@ -1,67 +1,77 @@
-function recommendations ()
-  var metabolicRate
-  document.getElementById("metabolicRate").innerHTML =
+var userData = JSON.parse(localStorage.getItem('userData'));
 
+  var goalForStatus = calculateCalories(userData.weights, userData.height, userData.age, userData.gender, userData.activityLevel);
+  var goalWeight = userData.goalWeight;
+  var loseOrGainFactor = userData.goal;
+  var weights = userData.weights;
+  var currentWeight = weights[weights.length - 1];
+  var texttodisplay = "";
 
-/*
-import java.util.Scanner;
+    // FIXME: In progress comparison currentWeight - Goal weight
+    function recommendationsBeta() {
+    if (loseOrGainFactor > 0) { //gaining weight
+        if ((goalWeight - currentWeight) > 3) {
+            texttodisplay = "Keep it up! Your almost reaching your goal.";
+        }
+        else if ((goalWeight - currentWeight) < 3) {
+            texttodisplay = "You need to work on your goal.";
+        }
+    }
+    else {
+       if ((currentWeight - goalWeight) < 3) {
+           texttodisplay = "Keep it up! Your almost reaching your goal.";
+       }
+       else if((currentWeight - goalWeight) < 3) {
+               texttodisplay = "You need to work on your goal.";
+       }
 
-public class TestRecommendation {
-public static void main (String [] arg) {
-
-  Scanner userInput = new Scanner (System.in);
-
-  int calculateCalories = 0;
-
-  System.out.print("Enter calculate calories: ");
-  calculateCalories = userInput.nextInt();
-
-  Recommendation recommend;
-  recommend = new Recommendation ();
-  recommend.recommendations(calculateCalories);
-  recommend.determine_recommend();
-
-  return;
+    }
   }
+function recommendations() {
+    // FIXME: 2000 cannot be goalForStatusTest because same variable cannot be compared
+    if (goalForStatus >= 1800 && goalForStatus < 2000) {
+      text = "Good";
+    }
+    else if  (goalForStatus >= 1500) {
+      text = "Medium";
+    }
+    else if (goalForStatus >= 1200 || goalForStatus < 1200) {
+      text = "Bad";
+    }
+
+    return text;
 }
-*/
 
-/*
-class Recommendation {
-	private int goal = 0;
-	private boolean weightGood = false;
-	private boolean weightMed = false;
-	private boolean weightBad = false;
+// TESTED WORKING! BODY MASS INDEX ALT. FOR RECOMMENDATION FUNCTION
+function BMI() {
+  var height = userData.height;
+  var weight = userData.weights;
 
+  // Convert height to centimeters to work with formula below
+  height = height * 30.48; // feet to centimeters
 
-	public void recommendations(int calculateCalories) {
+  // Convert weight from kilograms to pounds to work with formula below
+  weight = weight / 2.20462; // lbs. to kg
 
-		goal = calculateCalories;
+  return ((weight) / Math.pow((height/100), 2));
 
-		if (goal <= 1800 || goal <= goal) {
-			weightGood = true;
-		} else if (goal <= 1500 || goal <= 1800) {
-			weightMed = true;
-		} else if (goal <= 1200 || goal <= 1500) {
-			weightBad = true;
-		}
-
-	}
-
-	public void determine_recommend() {
-
-		System.out.println("Call working");
-
-		if (weightGood == true) {
-			System.out.println("Good");
-		}
-		else if (weightMed == true) {
-			System.out.println("Medium");
-		}
-		else if (weightBad == true) {
-			System.out.println("Bad");
-		}
-
-	}
 }
-*/
+
+// Source: https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/
+function bmiScale () {
+  var bmiValue = BMI();
+  var text = "";
+  if (bmiValue < 18.5) {
+      text = "Your BMI scale is considered underweight";
+  }
+  else if (bmiValue <= 24.9) {
+    text = "Your BMI scale is considered normal or healthy weight";
+  }
+  else if (bmiValue <= 25.0 || bmiValue <= 29.9) {
+    text = "Your BMI scale is considered overweight";
+  }
+  else if (bmiValue >= 30.0) {
+    text = "Your BMI scale is considered obese";
+  }
+  return text;
+}

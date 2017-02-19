ce261c3d38c3aeb9c81bda7db19b307e612c245a
legyuja <legyuja>
2017-02-18 19:08:36 -0800

    added front end files

--- script/newUser.js
+++ script/newUser.js
@@ -7,7 +7,6 @@ else
 
 var saveButton = document.querySelector('#save');
 var runButton = document.querySelector('#run');
-var resetButton = document.querySelector('#reset');
 var metabolicRate = document.querySelector('#metabolicRate');
 
 var weightInput = document.querySelector('#weight');
@@ -19,14 +18,8 @@ var goalWeightInput = document.querySelector('#goalWeight');
 
 saveButton.addEventListener("click", saveInput);
 runButton.addEventListener("click", run);
-resetButton.addEventListener("click", reset);
 
-function reset()
-{
-    localStorage.removeItem(userData);
-    window.location.replace(toTest.html);
-    console.log("reset button pressed");
-}
+
 
 function run()
 {
@@ -40,7 +33,6 @@ function run()
     userData.goal = userData.goalWeight - userData.weights[userData.weights.length - 1];
 
     metabolicRate.textContent = calculateCalories(userData.weights, userData.height, userData.age, userData.gender, userData.activityLevel);
-    recommendations(); // recommendation function
     saveInput();
     drecommendations.textContent = recommendations();
     bmiOutput.textContent = "Your Body Mass Index is: " + "\"" + BMI() + "\"";
@@ -96,57 +88,5 @@ function calculateCalories(weights, height, age, gender, activityLevel)
 // newWeight()
 
 // recommendations()
-function recommendations() {
-  var goalForStatus = calculateCalories(userData.weights, userData.height, userData.age, userData.gender, userData.activityLevel);
-  //var goalLimiter = userData.goalWeight;
-  var text = "";
-
-    // FIXME: 2000 cannot be goalForStatusTest because same variable cannot be compared
-    if (goalForStatus >= 1800 && goalForStatus < 2000) {
-      text = "Good";
-    }
-    else if  (goalForStatus >= 1500) {
-      text = "Medium";
-    }
-    else if (goalForStatus >= 1200 || goalForStatus < 1200) {
-      text = "Bad";
-    }
-
-    return text;
-}
-
-// TESTED WORKING! BODY MASS INDEX ALT. FOR RECOMMENDATION FUNCTION
-function BMI() {
-  var height = userData.height;
-  var weight = userData.weights;
-
-  // Convert height to centimeters to work with formula below
-  height = height * 30.48; // feet to centimeters
-
-  // Convert weight from kilograms to pounds to work with formula below
-  weight = weight / 2.20462; // lbs. to kg
-
-  return ((weight) / Math.pow((height/100), 2));
-
-}
-
-// Source: https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/
-function bmiScale () {
-  var bmiValue = BMI();
-  var text = "";
-  if (bmiValue < 18.5) {
-      text = "Your BMI scale is considered underweight";
-  }
-  else if (bmiValue <= 24.9) {
-    text = "Your BMI scale is considered normal or healthy weight";
-  }
-  else if (bmiValue <= 25.0 || bmiValue <= 29.9) {
-    text = "Your BMI scale is considered overweight";
-  }
-  else if (bmiValue >= 30.0) {
-    text = "Your BMI scale is considered obese";
-  }
-  return text;
-}
 
 // isReachGoal()
